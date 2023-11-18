4572ac1b9516283f4cbe315f75518ce58759f80d
hasibur-rafat <145573254+hasibur-rafat@users.noreply.github.com>
2023-11-18 11:56:35 +0600

    Module 8 Assignment

---
+++ ProfileController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donal Trump";
+        $age = "75";
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+        $response = new Response($data, 200);
+        $response->withCookie(cookie(
+            $cookieName,
+            $cookieValue,
+            $minutes,
+            $path,
+            $domain,
+            $secure,
+            $httpOnly
+        ));
+
+        return $response;
+    }
+}
